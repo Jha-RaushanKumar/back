7ffffcd844c723d4d85c9091bb7f107a3df2c2d9
Raushan Kumar Jha <jha.rau@northeastern.edu>
2022-12-08 13:32:52 -0500

    ff

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,11 @@ const cookieParser = require("cookie-parser")
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
+const cors = require('cors');
 
 //config 
 dotenv.config({ path: "backend/config/config.env" });
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
@@ -19,6 +21,16 @@ const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
 const payment = require("./routes/paymentRoute");
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
 
 app.use("/api/v1", product);
 app.use("/api/v1", order);
